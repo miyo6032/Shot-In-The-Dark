e0f362b19bd9707c21f089ef3aa454298584efaa
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-11 09:19:19 -0700

    Testing getting wwise audio working

---
+++ Assets/Scripts/Audio/AudioMusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class AudioMusic : MonoBehaviour
+{
+
+    // Wwise References //
+    [Header("Wwise Audio Events:")]
+    public AK.Wwise.Event FactoryAmbience;
+
+    private void Start()
+    {
+        FactoryAmbience.Post(gameObject);
+    }
+}
