65e9fdab62b074a7e919fabbac96dbfd962a89d7
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-15 07:08:34 -0700

    Add buildings; Add shadows

--- Assets/Scripts/Player/PlayerEmpAction.cs
+++ Assets/Scripts/Player/PlayerEmpAction.cs
@@ -1,12 +1,14 @@
 ﻿using Game_Service;
 using Game_Service.Services;
 using UnityEngine;
+using Event = AK.Wwise.Event;
 
 namespace Player
 {
     public class PlayerEmpAction : MonoBehaviour
     {
         [SerializeField] private PlayerInput playerInput;
+        [SerializeField] private Event empSoundEvent;
         
         private void Update()
         {
@@ -17,8 +19,9 @@ namespace Player
             }
         }
 
-        public static void DoEmp(IGameState gameStateService)
+        private void DoEmp(IGameState gameStateService)
         {
+            // empSoundEvent.Post(gameObject);
             if (gameStateService.GameState == GameState.Setup)
             {
                 gameStateService.GameState = GameState.EmpActivated;
