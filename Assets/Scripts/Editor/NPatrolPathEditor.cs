d8e9f2ef45ac45d93fa868bf459cffe19e7e56c9
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-18 14:05:43 -0700

    Bystander movement improved; improve pathing editor

--- Assets/Scripts/Editor/NPatrolPathEditor.cs
+++ Assets/Scripts/Editor/NPatrolPathEditor.cs
@@ -13,15 +13,23 @@ namespace Custom_Editors
             var path = target as NPatrolPath;
             using (var cc = new EditorGUI.ChangeCheckScope())
             {
-                var newPositions = path.nodes.Select(node =>
+                var transform = path.transform;
+                var newPositions = path.nodes.SkipLast(1).Select(node =>
                     path.transform.InverseTransformPoint(
-                        Handles.PositionHandle(path.transform.TransformPoint(node.position), path.transform.rotation))).ToList();
+                        Handles.PositionHandle(path.transform.TransformPoint(node.position), transform.rotation))).ToList();
+                var center = path.nodes.Aggregate(Vector3.zero, (vector3, node) => vector3 + (Vector3) node.position) / path.nodes.Count;
+                var mainPosition = Handles.PositionHandle(path.transform.TransformPoint(center), transform.rotation);
+                var offset = mainPosition - path.transform.TransformPoint(center);
+                
                 if (cc.changed) // calling .changed seems to affect state so have to get all position handles at once first
                 {
-                    for (var i = 0; i < path.nodes.Count; i++)
+                    for (var i = 0; i < path.nodes.Count - 1; i++)
                     {
-                        path.nodes[i].position = newPositions[i];
+                        path.nodes[i].position = newPositions[i] + offset;
                     }
+
+                    if (path.nodes.Count > 0)
+                        path.nodes[^1].position = path.nodes[0].position;
                 }
             }
         }
@@ -29,6 +37,7 @@ namespace Custom_Editors
         [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
         static void OnDrawGizmo(NPatrolPath path, GizmoType gizmoType)
         {
+            var transform = path.transform;
             for (int i = 0; i < path.nodes.Count - 1; i++)
             {
                 var start = path.transform.TransformPoint(path.nodes[i].position);
@@ -36,9 +45,15 @@ namespace Custom_Editors
                 Handles.color = Color.yellow;
                 Handles.DrawDottedLine(start, end, 5);
                 var forward = path.transform.forward;
+                Handles.color = Color.blue;
                 Handles.DrawSolidDisc(start, forward, 0.1f);
-                Handles.DrawSolidDisc(end, forward, 0.1f);
+                Handles.Label(start, $"{i} - Pause: {path.nodes[i].pause}");
             }
+
+            var center = path.nodes.Aggregate(Vector3.zero, (vector3, node) => vector3 + (Vector3) node.position) / path.nodes.Count;
+            
+            Handles.color = Color.red;
+            Handles.DrawSolidDisc(path.transform.TransformPoint(center), transform.forward, 0.2f);
         }
     }
 }
--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -17,7 +17,7 @@ namespace DefaultNamespace
             GameServiceProvider.RegisterServices(new BackendServices(new GameStateManager(), countdownTimer));
             FrontEndServiceProvider.RegisterServices(new FrontEndServices(lightingHandler));
             
-            GameServiceProvider.GetService<IGameTimer>().SetTimer(15, OnTimerOut, f => $"{f:0} seconds to make shot");
+            GameServiceProvider.GetService<IGameTimer>().SetTimer(30, OnTimerOut, f => $"{f:0} seconds to make shot");
             // GameServiceProvider.GetService<IGameTimer>().SetTimer(10, DoEmp, f => $"{f:0} seconds until EMP");
         }
         
