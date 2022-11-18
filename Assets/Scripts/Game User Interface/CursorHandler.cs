542d65984a637e9cd74451f72111e794a1e1b328
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-18 16:18:53 -0700

    Add custom aim cursor

---
+++ Assets/Scripts/Game User Interface/CursorHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Game_Service;
+using Game_Service.Services;
+using UnityEngine;
+
+namespace Game_User_Interface
+{
+    public class CursorHandler : MonoBehaviour
+    {
+        public Texture2D cursorTexture;
+        public CursorMode cursorMode = CursorMode.Auto;
+        public Vector2 hotSpot = Vector2.zero;
+        private void Start()
+        {
+            GameServiceProvider.GetService<IGameState>().AddGameStateChangeListener(SetCursor);
+        }
+
+        private void SetCursor(GameState gameState)
+        {
+            if (gameState == GameState.IsDark)
+            {
+                Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+            }
+        }
+    }
+}
