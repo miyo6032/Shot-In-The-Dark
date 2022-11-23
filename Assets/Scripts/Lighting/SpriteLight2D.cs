2ad68abd5625652bdee47a9761c3c8c1d9efb4d4
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-22 19:45:03 -0700

    Add emp shock FX to lamps

--- Assets/Scripts/Lighting/SpriteLight2D.cs
+++ Assets/Scripts/Lighting/SpriteLight2D.cs
@@ -5,10 +5,14 @@ namespace Lighting
     public class SpriteLight2D : LevelLight
     {
         [SerializeField] private SpriteRenderer sprite;
+        [SerializeField] private Animator empAnimator;
+        private static readonly int Emp = Animator.StringToHash("Emp");
+
         public override void TurnOff()
         {
             sprite.color = Color.black;
             gameObject.SetActive(false);
+            empAnimator.SetTrigger(Emp);
         }
     }
 }
