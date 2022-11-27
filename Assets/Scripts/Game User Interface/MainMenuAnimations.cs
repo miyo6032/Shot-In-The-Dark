b70d9f3ce6aae43425c2abc72578dee97acf7d5b
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-27 13:47:29 -0700

    Add main menu sprites + animations

---
+++ Assets/Scripts/Game User Interface/MainMenuAnimations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game_User_Interface
+{
+    public class MainMenuAnimations : MonoBehaviour
+    {
+        [SerializeField] private Image backgroundPanel;
+        [SerializeField] private CanvasGroup mainMenu;
+
+        private void Start()
+        {
+            LeanTween.value(gameObject, f => mainMenu.alpha = f, 0.0f, 1.0f, 1.0f);
+            backgroundPanel.color = Color.black;
+            LeanTween.value(gameObject, f => backgroundPanel.color = new Color(f, f, f, 1.0f), 0.0f, 1.0f, 1.0f).setDelay(1.0f);
+        }
+    }
+}
