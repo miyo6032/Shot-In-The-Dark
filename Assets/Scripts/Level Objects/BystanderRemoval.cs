45e73461e198346429cedcb682153dab4be223c7
Michael Yoshimura <miyo6032@colorado.edu>
2022-11-28 09:54:15 -0700

    Remove bystanders after off screen

---
+++ Assets/Scripts/Level Objects/BystanderRemoval.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Level_Objects
+{
+    public class BystanderRemoval : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Bystander"))
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    }
+}
